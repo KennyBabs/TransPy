41fbe29dc1ddeff9a268ccaaa5837067b456e0ce
princelewis <princelewis4christ@yahoo.com>
2019-09-06 21:08:00 +0100

    query terminals table for depatures

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required,luhn_algorithm
+from helpers import apology, login_required,luhn_algorithm, naira
 
 #configure application
 app = Flask(__name__)
@@ -34,7 +34,8 @@ db = SQL("sqlite:///transpy.db")
 @login_required #this needs to be implemented on helpers.py
 def index():
     #return apology("you need to work on the booking page", 400)
-    return render_template("booking.html")
+    depatures = db.execute("SELECT arrival FROM terminals WHERE depature=Lagos")
+    return render_template("booking.html", depatures=depatures)
 
 @app.route("/login", methods=["GET","POST"])
 def login():
@@ -157,6 +158,8 @@ def seatselect():
     info = request.get_json()
 
     print(info)
+
+
 
 
 def errorhandler(e):
