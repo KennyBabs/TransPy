38827d245bfab1784606fc4ae0e86227cab523ee
CodeVee <victorchike247@gmail.com>
2019-09-10 07:56:16 +0100

    Updated stylesheet

--- static/transpy.js
+++ static/transpy.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $('#dest-state').change(function(){
 
         let arrival = $('#dest-state option:selected').text()
-
+        
         $.get("/checkprice?arrival="+arrival, function(data){
             $('#amount p').html(data);
             $('.amountFee').show();   
